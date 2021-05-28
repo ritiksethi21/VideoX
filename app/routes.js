1766f7b0aad46b132adc65ca6e5b5ec71dc81e7c
Ritik Sethi <58940080+ritiksethi21@users.noreply.github.com>
2021-05-28 18:29:50 +0530

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,4 @@ const controller = require('./controller');
 
 router.post("/api/save-call-id", controller.saveCallId);
 router.get("/api/get-call-id/:id", controller.getCallId);
-
 module.exports = router;
